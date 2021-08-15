09b5e79001a30e23006996c21e015cbf5f44b968
Sebastian Pieczyński <testament777@gmail.com>
2021-08-15 07:30:30 +0200

    feat: ✨ User is fetched with Prisma
    Fixed all the issue from yesterday, user is passed to GraphQL server with correct typings as well

--- src/resolvers/User/user.resolver.ts
+++ src/resolvers/User/user.resolver.ts
@@ -1,27 +1,36 @@
-import { IResolvers } from 'mercurius';
+import { IResolvers } from "mercurius";
 
-import { User as PrismaUser } from '@prisma/client';
+import { User as PrismaUser } from "@prisma/client";
 
 // import { User } from '../../generated/typings/generatedTypings';
 
 const resolvers: IResolvers = {
 	Query: {
-		user(root, { userId }, ctx, info) {
-			//TODO:fix from any to Prisma user..
-			const userData: any = ctx.prisma.user.findUnique({
+		user: async (root, { userId }, ctx, info) => {
+			let result = null;
+			console.log(`User ID: ${userId}`);
+
+			const userData: PrismaUser | null = await ctx.prisma.user.findUnique({
 				where: { id: userId! },
 			});
 
-			const user: any = {
-				id: userData.id,
-				email: userData.email,
-				login: userData.login,
-			};
+			if (userData) {
+				const user: any = {
+					id: userData.id,
+					email: userData.email,
+					login: userData.login,
+				};
+				result = user;
+			} else {
+				console.error(`User with ID: ${userId} not found.`);
+				throw new Error("User not found");
+			}
+
 			// root ~ {}
 			// args ~ variables passed to query
 			// ctx.authorization ~ string | undefined
 			// info ~ GraphQLResolveInfo
-			return user;
+			return result;
 		},
 	},
 };
