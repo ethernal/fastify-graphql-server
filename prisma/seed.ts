b28bf26bae8c185c895bfbc6f112a7eaa61e8da5
Sebastian Pieczyński <testament777@gmail.com>
2021-08-15 08:00:15 +0200

    fix: 🐛 Fix: seedin hanged due to async function in finally statement
    Improved seed reliability for PostgreSQL

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,29 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-import { users } from './users';
+import { users } from "./users";
 
 const prisma = new PrismaClient();
 
 async function seed() {
 	try {
+		console.log("Starting DB Seeding");
+
 		// Prisma create query to seed models in database
 		// console.table(users);
 
-		await prisma.user.createMany({
+		const response = await prisma.user.createMany({
 			data: users,
+			skipDuplicates: true,
 		});
+
+		console.log(`Affected rows: ${response.count}`);
+		console.log("Done DB Seeding");
 	} catch (e) {
 		console.error(e);
 		process.exit(1);
 	} finally {
-		async () => {
-			await prisma.$disconnect();
-			process.exit(0);
-		};
+		await prisma.$disconnect();
+		console.log("Disconnected Prisma!");
 	}
 }
 
