1e2bfdf08dd3fa413506197a65bd43be202d52bd
Sebastian Pieczyński <testament777@gmail.com>
2021-08-18 21:54:36 +0200

    fix: 🎨 Schema bugs fixed and more robust, API and user resolver updated
    User resolver correctly typed and properly returned.
    Schema bugs fixed and prepared for more features.

--- src/resolvers/User/user.resolver.ts
+++ src/resolvers/User/user.resolver.ts
@@ -1,34 +1,27 @@
-import { IResolvers } from "mercurius";
+import { IResolvers } from 'mercurius';
 
-import { User as PrismaUser } from "@prisma/client";
+import { User } from '@prisma/client';
 
 const resolvers: IResolvers = {
 	Query: {
 		user: async (root, { userId }, ctx, info) => {
 			let result = null;
 
-			const userData: PrismaUser | null = await ctx.prisma.user.findUnique({
+			const userData: User | null = await ctx.prisma.user.findUnique({
 				where: { id: userId! },
 			});
 
-			if (userData) {
-				const user: any = {
-					id: userData.id,
-					email: userData.email,
-					login: userData.login,
-					createdAt: userData.createdAt,
-				};
-				result = user;
-			} else {
+			if (!userData) {
 				console.error(`User with ID: ${userId} not found.`);
 				throw new Error("User not found");
 			}
+
+			return userData;
 
 			// root ~ {}
 			// args ~ variables passed to query
 			// ctx.authorization ~ string | undefined
 			// info ~ GraphQLResolveInfo
-			return result;
 		},
 	},
 };
